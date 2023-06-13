ce7d2db0dac0601b17bc5036323d063f9b2aaee7
Lamber22 <Lamberjoe5@gmail.com>
2023-06-13 09:17:45 -0700

    Fixed pycodestyle error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """program that prints all the names defined by the compiled module 
-hidden_4.pyc"""
+    """program that prints all the names defined by
+    the compiled module hidden_4.pyc"""
     import hidden_4
 
     names = dir(hidden_4)
