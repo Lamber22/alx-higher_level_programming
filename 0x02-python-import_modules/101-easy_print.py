091fd489f65fc9fb88a4d10ea08dc42df7d86ee8
Lamber22 <Lamberjoe5@gmail.com>
2023-06-13 15:48:54 -0700

    Fixing unknown eror

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -2,4 +2,5 @@
 
 """This function prints to stdout with the write function"""
 
-__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+import builtins
+builtins.print("#pythoniscool")
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     """program that imports the variable a from the file variable_load_5.py and
     prints its value"""
-    from variable_load_5 import a
+    import variable_load_5
 
     print(a)
