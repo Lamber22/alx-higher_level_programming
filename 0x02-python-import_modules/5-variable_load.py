e8b6de7164784deb15666b2ab9df38b790f960a8
Lamber22 <Lamberjoe5@gmail.com>
2023-06-19 06:39:52 -0700

    fixin gunknown error

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     """program that imports the variable a from the file variable_load_5.py and
     prints its value"""
-    import variable_load_5
+    from variable_load_5 import a
 
     print(a)
