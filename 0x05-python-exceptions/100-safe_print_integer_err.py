94e6f5d30624bb2809dff0f19a7dfdec04f42c75
Lamber22 <Lamberjoe5@gmail.com>
2023-06-26 14:06:24 -0700

    pycodestyle error fixed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,12 +3,11 @@
 def safe_print_integer_err(value):
     import sys
 
-
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
-    
+
     else:
         return True
