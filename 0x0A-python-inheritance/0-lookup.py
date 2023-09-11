57a3c880c5fab1786c7c784dfb4bed1e426458eb
Lamber22 <Lamberjoe5@gmail.com>
2023-09-11 14:47:43 -0700

    Fixed pycodestyle error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Defines an object attribute function"""
+"""Defines an object attribute function."""
+
 
 def lookup(obj):
     """This function return a list of an object available attributes"""
