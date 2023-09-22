97ced25c86253e854130b70661c8467da693e2ec
Lamber22 <Lamberjoe5@gmail.com>
2023-09-21 17:15:55 -0700

    created a Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+"""Defines a Base class"""
+
+class Base:
+    """Represents a base model
+
+    Attributes:
+        __nb_objects (int): The number of instanctiated Bases.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes a new base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
