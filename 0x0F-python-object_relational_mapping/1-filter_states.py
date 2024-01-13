1f4f2cd12dbeda7438941b1164663b1465895dad
Lamber22 <Lamberjoe5@gmail.com>
2024-01-13 13:58:10 -0800

    Lists all states with name starting with (N)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * 'states' WHERE 'name' LIKE 'N'"
+    query = "SELECT * FROM 'states' WHERE 'name' LIKE 'N' ORDER BY 'id'"
     cur.execute(query)
     states = cur.fetchall()
     for state in states:
