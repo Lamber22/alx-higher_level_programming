2f95fe96c87900e9c9fe1052ac230fa708732c98
Lamber22 <Lamberjoe5@gmail.com>
2024-02-20 18:28:31 -0800

    Computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-//Computes the number of tasks completed by user id.
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
