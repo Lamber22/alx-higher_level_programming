e30348ca8ccbadc8d230dc7169b4a88fcb213fa4
Lamber22 <Lamberjoe5@gmail.com>
2024-02-29 00:16:03 +0000

    10. No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Sets the text color to red when loaded
+
+document.addEventListener("DOMContentLoaded", function() {
+    let header = document.querySelector("header");
+    header.style.color = "#FF0000"; 
+});
